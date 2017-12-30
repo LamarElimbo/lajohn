901e3f4f8ebdef3c26714ace3f5777dfca39c527
lamar133 <lamar_johnson133@yahoo.ca>
2017-12-29 20:10:16 -0500

    cache busted css files

--- twata/views.py
+++ twata/views.py
@@ -6,7 +6,7 @@ import os
 
 
 def home(request):
-    return render(request, 'twata/home.html', {'css_source': '../static/home2_static/app.css', 'activeTab': 'home'})
+    return render(request, 'twata/home.html', {'css_source': '../static/home2_static/app_v1.css', 'activeTab': 'home'})
 
 
 def graph(request):
@@ -16,4 +16,4 @@ def graph(request):
     pos_scores, pos_height = getGraphScript.getPosScript()
     x_keys = getGraphScript.getXCats()
 
-    return render(request, 'twata/graph.html', {'css_source': 'static/twata_static/app.css', 'date': 'Tuesday, November 3, 2017', 'neg_latest_scores_ordered': neg_scores, 'neg_graph_height': neg_height, 'pos_latest_scores_ordered': pos_scores, 'pos_graph_height': pos_height, 'x_cats': x_keys})
+    return render(request, 'twata/graph.html', {'css_source': 'static/twata_static/app_v1.css', 'date': 'Tuesday, November 3, 2017', 'neg_latest_scores_ordered': neg_scores, 'neg_graph_height': neg_height, 'pos_latest_scores_ordered': pos_scores, 'pos_graph_height': pos_height, 'x_cats': x_keys})
